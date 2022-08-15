a3eabf7cefbb8f78010a6c7d6945d504c70878b5
Lopez-CL <cllopez511@gmail.com>
2022-08-15 07:00:54 -0400

    adding parts one and two

---
+++ loops_Challenge_Lopez.js
@@ -0,0 +1,21 @@
+// Print odds 1-20 Using a loop write code that will console.log all of the odd values from 1 up to 20.//
+
+function printOdds(){
+    for (var i = 1; i <= 19; i++){
+        if (i % 2 != 0){
+            console.log(i);
+        }
+    }
+}
+console.log(printOdds());
+
+// Using a loop write code that will console.log all of the values that are evenly divisible by 3 from 100 down to 0.//
+
+function multiplesOfThree(){
+    for (var i = 100; i > -1; i--){
+        if (i % 3 == 0){
+            console.log(i);
+        }
+    }
+}
+console.log(multiplesOfThree());
